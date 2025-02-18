a5870bd8a31e85bc5d80ae0874e4bd880f7b693f
mangsgi <aodrntjr0901@naver.com>
2025-02-18 19:56:27 +0900

    fix: change git actions period and add comments

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
-# 인공지능 관련 뉴스 전달 디스코드 웹훅
+# 인공지능 관련 뉴스 전달 (Discord Webhook)
 
-## how to use
-.env 파일 생성
-`DISCORD_URL=<디스코드 채널 웹 훅 URL>`
+> 이 스크립트는 **[GeekNews](https://news.hada.io/new) 페이지**에서 AI 관련 뉴스를 스크래핑하여 Discord Webhook을 통해 특정 Discord 채널로 알림을 전송합니다.
 
-github action cron
+## 기능
+
+- Git Actions을 통해 매일 오전 10시에 main.py 실행
+- Python 라이브러리인 BeautifulSoup를 사용하여 GeekNews에서 24시간 이내 올라온 뉴스 확인
+- 뉴스 내용 중에 AI 관련 용어가 포함되었는지 확인 후 스크래핑
+- 뉴스 제목과 링크를 Discord로 전송
+
+## 사용 방법
+
+1. .env 파일 생성
+
+```bash
+DISCORD_URL=<YOUR_DISCORD_WEB_HOOK_URL>
+```
+
+2. 요구사항 설치
+
+```bash
+pip intsall -r requirments.txt
+```
+
+## 출력 예시
+
+```
+## 📎Flash AID - LLM을 모든 파이프라인에 연동하기
+
+ 링크 : https://news.hada.io/topic?id=12345
+## 📎Open-AID: LLM으로 컴퓨터를 제어하기
+
+ 링크 : https://news.hada.io/topic?id=67890
+## 📎Show AID: AID AI - 반응형 GPT 웹 인터페이스
+
+ 링크 : https://news.hada.io/topic?id=00000
+```
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import os  # os모듈 불러오기 (운영체제와의 상호작용을 돕는 다양한 기능 제공 )
+import os  # 운영체제와의 상호작용을 돕는 다양한 기능 제공
 
-import requests  # requests모듈 불러오기(http, https 웹 사이트에 요청하기 위해 자주 사용되는 모듈)
+import requests  # http, https 웹 사이트에 요청하기 위해 자주 사용되는 모듈
 from bs4 import (
     BeautifulSoup,  # 웹 데이터 크롤링 도는 스크래핑을 할 때 사용하는 python 라이브러리
 )
 from dotenv import load_dotenv  # .env 파일에서 환경 변수를 로드하는 라이브러리
 
 load_dotenv(dotenv_path=".env")  # 현재 작업 디렉토리에 있는 .env 파일에서 환경 변수를 로드
-DISCORD_URL = os.getenv("DISCORD_URL")  # os.getenv()는 함수는 지정된 환경 변수의 값을 반환하거나, 해당 환경 변수가 존재하지 않으면 None을 반환
-GEEKNEWS_BASEURL = "https://news.hada.io/"  #
-GEEKNEWS_URL = "https://news.hada.io/new"
-want_to_include = ["AI", "ai", "ML", "GPT", "LLM", "Diffusion", "인공지능", "딥러닝", "머신러닝", "사전학습", "파인튜닝"]
-days_included = [f"{i}분전" for i in range(1, 20)]
 
+DISCORD_URL = os.getenv("DISCORD_URL")  # os.getenv()는 함수는 지정된 환경 변수의 값을 반환하거나, 해당 환경 변수가 존재하지 않으면 None을 반환
+GEEKNEWS_BASEURL = "https://news.hada.io/"
+GEEKNEWS_URL = "https://news.hada.io/new"
+want_to_include = ["AI", "ai", "ML", "GPT", "LLM", "Diffusion", "인공지능", "딥러닝", "머신러닝", "사전학습", "파인튜닝"] # AI 관련 뉴스인지 판단하기 위한 지표
+days_included = [f"{i}시간전" for i in range(1, 23)] # 24시간 내 뉴스인지 확인
 
 headers = {"Content-Type": "application/json"}
 messages = []
@@ -22,51 +22,59 @@ if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
     topics = soup.find_all("div", class_="topic_row")
+    
     for topic in topics:
-        topic_title = topic.find("div", class_="topictitle")
-        topic_content = topic.find("div", class_="topicdesc")
-        topic_url = topic.find("span", class_="topicurl")
-        topic_info = topic.find("div", class_="topicinfo")
-        topic_date = topic_info.text.split(" ")[4]
-
+        # 뉴스 내 tag 지정
+        topic_title = topic.find("div", class_="topictitle") # 뉴스 제목
+        topic_content = topic.find("div", class_="topicdesc") # 뉴스 내용
+        topic_url = topic.find("span", class_="topicurl") # 뉴스 링크
+        topic_info = topic.find("div", class_="topicinfo") # 뉴스 정보
+        
+        # 뉴스 제목 추출 
         if topic_title.a is None:
             title = topic_title.h1.string
         else:
             title = topic_title.a.h1.string
             # topic_link = topic_title.find("a")["href"].strip()
-
+        
+        # 뉴스 내용 추출
         description = ""
         if topic_content is not None:
             description = topic_content.a.string
             topic_link = topic_content.find("a")["href"].strip()
         else:
             topic_link = topic_title.find("a")["href"].strip()
-        flag = False
 
+        # 뉴스 url이 없으면 건너뀜
         if topic_url.string is None:
             continue
 
+        # 뉴스 날짜 추출해서 24시간 내 내용인지 확인
+        date_flag = False
+        topic_date = topic_info.text.split(" ")[4]
         for days in days_included:
             if days == topic_date:
-                flag = True
+                date_flag = True
                 break
-
-        if not flag:
+            
+        # 24시간이 지난 뉴스는 건너뜀
+        if not date_flag:
             continue
 
+        # AI 관련 뉴스인지 확인
         for word in want_to_include:
             if word in title or word in description:
                 message = f"## 📎{title}\n\n 링크 : {GEEKNEWS_BASEURL + topic_link}"
                 messages.append(message)
                 break
-
 else:
     print(f"Error: {response.status_code}")
 
-
+# 디스코드 전송
 if len(messages) != 0:
     for message in messages:
         data = {"content": message}
         response = requests.post(DISCORD_URL, json=data)
+        # print(message)
         if response.status_code == 404:
             print("response code is 404, check discord url or action variable")
